3ebaa80c56c355c11161b08adfae1370a4e07a16
Tika Oliver <132230087+tika1993@users.noreply.github.com>
2023-11-19 16:05:33 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,12 @@ export default defineConfig({
   // If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/<USERNAME>, set base to '/'.
 
   // If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/, for example your repository is at https://github.com/<USERNAME>/<REPO_NAME>, then set base to '/<REPO_NAME>/'.
-
+// vite.config.js
+  
+{
+  base: '/',
+  // ...
+}                           
   base: '/gitprofile/',
   plugins: [
     react(),
